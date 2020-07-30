83d3e28d34d0c006cc1c9957235d3df696211730
cjgmj <cjgmj@hotmail.com>
2020-07-30 14:49:42 +0200

    Comentarios en Dart

---
+++ 01-basico/04-comentarios.dart
@@ -0,0 +1,23 @@
+/**
+ * Comentario en bloque para documentar el código
+ */
+main() {
+  // Comentario de una sola línea
+  int a = 10; // a debería de ser 10
+
+  /* Comentario en bloque
+  final personas = [
+    // Esta es la primera persona
+    'Joe', // [0]
+    // Esta es la segunda persona
+    'John', // [1]
+    // Esta es la tercera persona
+    'Linus' // [2]
+  ];
+  */
+}
+
+/// Comentario para documentar el código
+/// Función saludar
+/// Recibe un [nombre] y se concatena con el [mensaje]
+saludar(String nombre, String mensaje) {}
