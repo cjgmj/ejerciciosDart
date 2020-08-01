eb235ff0ec1ca6661ea9338fc2e05ebd8e9732d8
cjgmj <cjgmj@hotmail.com>
2020-08-01 13:15:20 +0200

    Queue

---
+++ 04-tipos-no-comunes/01-queue.dart
@@ -0,0 +1,16 @@
+import 'dart:collection';
+
+main() {
+  Queue<int> cola = new Queue();
+
+  cola.addAll([10, 20, 30, 40, 50]);
+
+  // print(cola is Queue);
+  // print(cola);
+
+  Iterator i = cola.iterator;
+
+  while (i.moveNext()) {
+    print(i.current);
+  }
+}
