4b6c3ed7077b259c2e5c439d47b9e67cc7c70609
cjgmj <cjgmj@hotmail.com>
2020-08-04 15:03:07 +0200

    Extraer body de la petición

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -2,8 +2,17 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import 'package:paquetes/paquetes.dart' as paquetes;
+
 void main(List<String> arguments) {
   final url = 'https://reqres.in/api/users?page=2';
 
-  http.get(url).then((resp) => print(resp));
+  http.get(url).then((resp) {
+    final body = jsonDecode(resp.body);
+
+    print(body);
+    print('Page: ${body['page']}');
+    print('Per_page: ${body['per_page']}');
+    print('Id del tercer elemento: ${body['data'][2]['id']}');
+  });
 }
